df3a6ea0f7701346ba14202cf71baed15bdf9d83
samirls <samirlaguardiaii@gmail.com>
2023-06-16 17:44:39 -0300

    Initial commit

---
+++ src/main/java/com/example/todofamilyapi/TodoFamilyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.todofamilyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoFamilyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoFamilyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todofamilyapi/TodoFamilyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todofamilyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoFamilyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
